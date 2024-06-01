18848b72fe1c737bad18bbb49e120545bd90b3d7
Johans Bill <johans.bill.d.marva@gmail.com>
2024-06-02 07:13:14 +0800

    test approval pull request

--- src/main/java/com/travelonline/git/GitApplication.java
+++ src/main/java/com/travelonline/git/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
 		/*test pull request*/
+		/*test pull request 2*/
 	}
 
 }
